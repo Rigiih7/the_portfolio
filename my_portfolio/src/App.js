9435a81784e3c17d86afacb564f198fb815666f2
Rigiih7 <mwirigihbrian@gmail.com>
2024-02-11 20:56:22 +0300

    display Github profile and repositories

---
+++ my_portfolio/src/App.js
@@ -0,0 +1,116 @@
+//import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Button, Card, CardGroup } from 'react-bootstrap';
+
+function App() {
+    const [avatarURL, setAvatarURL] = useState();
+    const [avatar1URL, setAvatar1URL] = useState ();
+    const [githubUsername, setGitHubUsername] = useState();
+    const [githubUsername1, setGitHubUsername1] = useState();
+    const [repoData, setRepoData] = useState();
+    const [repoData1, setRepoData1] = useState();
+
+  async function repoDataURL() {
+    //Get repo data about github user
+    await fetch("https://api.github.com/users/Rigiih7/repos")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(36, result);
+          const list = result.map((item) => (
+            <div className="text-center">
+              <a target="_blank" href={item.svn_url}>
+                {item.name}
+              </a>
+            </div>
+          ));
+          setRepoData(list);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+  async function repoData1URL() {
+    //Get repo data about github user
+    await fetch("https://api.github.com/users/TamaraLumumba/repos")
+      .then((res1) => res1.json())
+      .then(
+        (result1) => {
+          console.log(36, result1);
+          const list = result1.map((item1) => (
+            <div className="text-center">
+              <a target="_blank" href={item1.svn_url}>
+                {item1.name}
+              </a>
+            </div>
+          ));
+          setRepoData1(list);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+
+    useEffect(() => {
+      fetch("https://api.github.com/users/Rigiih7")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setAvatarURL(result.avatar_url);
+          setGitHubUsername(result.login);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+
+      fetch("https://api.github.com/users/TamaraLumumba")
+      .then((res1) => res1.json())
+      .then(
+        (result1) => {
+          console.log(result1);
+          setAvatar1URL(result1.avatar_url); // Fix: Use result1 for the second user
+          setGitHubUsername1(result1.login); // Fix: Use result1 for the second user
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+
+    }, []);
+    return (
+    <div className='App w-100 min-vh-100 justify-content-center align-items-center d-flex flex-column'>
+    <CardGroup>
+    <Card style={{ width: '18rem' }} className='mb-3 p-4 card-spacing border-5'>
+      <Card.Img variant="top" src={avatarURL} />
+      <Card.Body>
+        <Card.Title>
+          {githubUsername}
+        </Card.Title>  
+        <Button variant="primary" onClick = {repoDataURL}>List my repositories!</Button>
+      </Card.Body>
+      {repoData}
+    </Card>
+    <Card style={{ width: '18rem' }} className='mb-3 p-4 card-spacing border-5'>
+      <Card.Img variant="top" src={avatar1URL} />
+      <Card.Body>
+        <Card.Title>
+          {githubUsername1}
+        </Card.Title>
+        
+        <Button variant="primary" onClick = {repoData1URL}>List my repositories1!</Button>
+      </Card.Body>
+      {repoData1}
+      </Card> 
+      </CardGroup>
+    </div>
+  );
+
+}
+
+export default App;
